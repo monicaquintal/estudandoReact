8bac677770eecebf5dbc2f7b3bbe7d3bfb00081e
monicaquintal <monica.zoom@hotmail.com>
2023-07-07 23:15:44 -0300

    aula 03

---
+++ reactjs/01-github-explorer/dist/bundle.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+// const user = {
+//   name: 'Mônica',
+// }
+
+// console.log(user.address?.street);
+
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World! \uD83C\uDF0E");
+}
---
+++ reactjs/01-github-explorer/src/index.jsx
@@ -0,0 +1,11 @@
+// const user = {
+//   name: 'Mônica',
+// }
+
+// console.log(user.address?.street);
+
+import React from 'react';
+
+function App() {
+  return <h1>Hello World! 🌎</h1>
+}
